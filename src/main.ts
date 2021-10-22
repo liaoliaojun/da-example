fea41e717852496a35eb858da776d580b4535752
liaoliaojun <me@liaoliaojun.com>
2021-10-22 11:40:26 +0800

    fix: @apply

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import {createApp} from 'vue'
 import router from './router/'
 import App from './app.vue'
-import './styles/index.css'
+import './styles/index.postcss'
 // import 'element-plus/dist/index.css'
-// import './styles/element/reset-element-ui.postcss'
+import './styles/element/reset-element-ui.postcss'
 
 createApp(App).use(router).mount('#app')
