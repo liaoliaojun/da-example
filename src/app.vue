ad72c7f8eaf159cc20c6b0c4fc8844239714cf4e
liaoliaojun <me@liaoliaojun.com>
2021-10-29 18:17:18 +0800

    feat: 菜单调整

--- src/app.vue
+++ src/app.vue
@@ -2,10 +2,14 @@
   <el-config-provider :locale="locale">
     <ul class="flex ml-5 mt-3 theme-text">
       <router-link to="/" tag="li" class="mr-4">首页</router-link>
-      <router-link to="/base" tag="li" class="mr-4">基础资产</router-link>
-      <router-link to="/test" tag="li" class="mr-4">指标资产</router-link>
-      <router-link to="/report" tag="li" class="mr-4">非结构化资产</router-link>
-      <router-link to="/test" tag="li" class="mr-4">资产查询</router-link>
+      <router-link to="/base" tag="li" class="mr-4">基础资产管理</router-link>
+      <router-link to="/report" tag="li" class="mr-4">报表资产管理</router-link>
+      <router-link to="/test" tag="li" class="mr-4">接口资产管理</router-link>
+
+      <router-link to="/test" tag="li" class="mr-4">基础资产查询</router-link>
+      <router-link to="/test" tag="li" class="mr-4">指标资产查询</router-link>
+      <router-link to="/test" tag="li" class="mr-4">报表资产查询</router-link>
+      <router-link to="/test" tag="li" class="mr-4">接口资产查询</router-link>
     </ul>
     <div class="p-4 flex flex-col flex-1">
       <router-view />
